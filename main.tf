70de55eaddc57f338f0dfc3fc65f0620d051178a
Rajkumar <rajkumarpatel52247@gmail.com>
2023-06-12 06:58:06 +0530

    s

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "aws_ssm_parameter" "param" {
+  count = lenght(var.params)
+  name  = var.params[count.index].name
+  type  = var.params[count.index].value
+  value = var.params[count.index].type
+}
+
+variable "params" {
+  default = [
+    { name = "demo1", value = "demo1", type = "string"},
+    { name = "demo2", value = "demo2", type = "string"}
+  ]
+}
