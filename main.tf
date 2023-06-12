d37008eeff2df13ab9fb84bd29d6cc6aaf7bd768
Rajkumar <rajkumarpatel52247@gmail.com>
2023-06-12 11:16:17 +0530

    s

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "param" {
-  count = lenght(var.params)
+  count = length(var.params)
   name  = var.params[count.index].name
   type  = var.params[count.index].value
   value = var.params[count.index].type
