b1deba6d432a60842df86d1531328358f6e5f6ca
Rajkumar <rajkumarpatel52247@gmail.com>
2023-06-28 11:08:13 +0530

    s

--- main.tf
+++ main.tf
@@ -31,6 +31,13 @@ variable "params" {
     { name = "roboshop.dev.user.mongo", value = "true", type = "String" },
     { name = "roboshop.dev.user.redis_host", value = "redis-dev.devops73.online", type = "String" },
     { name = "roboshop.dev.user.mongo_url", value = "smongodb://mongodb-dev.devops73.online:27017/user", type = "String" },
+    { name = "roboshop.dev.rabbitmq.amqp_user", value = "roboshop", type = "String" },
+    { name = "roboshop.dev.rabbitmq.amqp_pass", value = "roboshop123", type = "String" },
+    { name = "roboshop.dev.mysql.username", value = "roboshop", type = "String" },
+    { name = "roboshop.dev.mysql.password", value = "roboshop123", type = "String" },
+    { name = "roboshop.dev.docdb.username", value = "roboshop", type = "String" },
+    { name = "roboshop.dev.docdb.password", value = "roboshop123", type = "String" },
+
 
   ]
 }
